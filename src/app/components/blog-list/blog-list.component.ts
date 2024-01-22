cfa04ec2ea7d9e5741c9a9ef1c58ba1e6e41e271
Muno Gwata <munozovepi@astute.money>
2024-01-22 16:38:29 +0200

    not working

--- src/app/components/blog-list/blog-list.component.ts
+++ src/app/components/blog-list/blog-list.component.ts
@@ -1,4 +1,8 @@
+// blog-list.component.ts
+
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { ContentfulService } from 'src/app/services/contentful.service';
 
 @Component({
@@ -11,12 +15,22 @@ export class BlogListComponent implements OnInit {
 
   constructor(private contentfulService: ContentfulService) { }
 
-// Inside BlogListComponent
-ngOnInit(): void {
-  this.contentfulService.getBlogPosts().subscribe(
-    posts => this.blogPosts = posts,
-    error => console.error('Error fetching blog posts in BlogListComponent:', error)
-  );
-}
+  ngOnInit(): void {
+    this.contentfulService.getBlogPosts().subscribe(
+      posts => this.blogPosts = posts,
+      (error: any) => console.error('Error fetching blog posts in BlogListComponent:', error) // Specify the type of 'error'
+    );
+  }
 
+  // Inside BlogListComponent
+  getPostThumbnailUrl(post: any): Observable<string> {
+    const assetId = post.fields.thumbnail.sys.id;
+    return this.contentfulService.getAssetById(assetId).pipe(
+      map((asset: { fields: { file: { url: string }; }; }) => asset.fields.file.url), // Specify the type of 'url'
+      catchError((error: any) => {
+        console.error(`Error fetching thumbnail URL for asset with ID ${assetId}:`, error);
+        throw error;
+      })
+    );
+  }
 }
--- src/app/services/contentful.service.ts
+++ src/app/services/contentful.service.ts
@@ -1,6 +1,8 @@
+// contentful.service.ts
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
@@ -8,23 +10,38 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class ContentfulService {
   private apiUrl = 'https://cdn.contentful.com/spaces/j1lug1a1cve1/environments/master/entries';
-  private accessToken = '92nr7MTk0rbJyV2MxX4rFHcYZbvVrGD-qN3pIhSgXz4'
-  ;
+  private accessToken = '92nr7MTk0rbJyV2MxX4rFHcYZbvVrGD-qN3pIhSgXz4';
 
   constructor(private http: HttpClient) { }
 
-// Inside ContentfulService
-getBlogPosts(): Observable<any[]> {
-  const url = `${this.apiUrl}?content_type=blogPosts&access_token=${this.accessToken}`;
-  return this.http.get<any>(url).pipe(
-    map(response => response.items),
-    tap(data => console.log('Blog Posts:', data)), // Log the data
-    catchError(error => {
-      console.error('Error fetching blog posts:', error);
-      throw error;
-    })
-  );
-}
+  getAccessToken(): string {
+    return this.accessToken;
+  }
+
+  // Inside ContentfulService
+  getBlogPosts(): Observable<any[]> {
+    const url = `${this.apiUrl}?content_type=blogPosts&include=1&access_token=${this.accessToken}`;
+    return this.http.get<any>(url).pipe(
+      map(response => response.items),
+      tap(data => console.log('Blog Posts:', data)), // Log the data
+      catchError(error => {
+        console.error('Error fetching blog posts:', error);
+        throw error;
+      })
+    );
+  }
+
+  // Add a new method to fetch asset details by ID
+  getAssetById(assetId: string): Observable<any> {
+    const assetUrl = `https://cdn.contentful.com/spaces/j1lug1a1cve1/assets/${assetId}?access_token=${this.accessToken}`;
+    return this.http.get<any>(assetUrl).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error(`Error fetching asset with ID ${assetId}:`, error);
+        throw error;
+      })
+    );
+  }
 
   getBlogPostById(id: string): Observable<any> {
     const url = `${this.apiUrl}/${id}?access_token=${this.accessToken}`;
@@ -32,19 +49,7 @@ getBlogPosts(): Observable<any[]> {
       map(response => response.fields),
       catchError(error => {
         console.error(`Error fetching blog post with ID ${id}:`, error);
-        throw error;
-      })
-    );
-  }
-
-  getAssets(): Observable<any[]> {
-    const url = `https://cdn.contentful.com/spaces/j1lug1a1cve1/assets?access_token=${this.accessToken}`;
-    return this.http.get<any>(url).pipe(
-      map(response => response.items),
-      tap(data => console.log('Assets:', data)), // Log the data
-      catchError(error => {
-        console.error('Error fetching assets:', error);
-        throw error;
+        return throwError(error);
       })
     );
   }
