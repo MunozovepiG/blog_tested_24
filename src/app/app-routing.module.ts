058f8a2a52ccc1d178a240e5be35f56df15a5ca6
Muno Gwata <munozovepi@astute.money>
2024-02-10 11:39:22 +0400

    header completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogListComponent } from './components/blog-list/blog-list.component';
 import { BlogPostComponent } from './components/blog-post/blog-post.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { RoadmapComponent } from './pages/roadmap/roadmap.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   { path: 'blog', component: BlogListComponent },
   { path: 'blog/:id', component: BlogPostComponent },
+  {path: '', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path: 'roadmap', component:RoadmapComponent },
+  {path: 'contact_us', component: ContactComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,28 @@ import { AppComponent } from './app.component';
 import { BlogListComponent } from './components/blog-list/blog-list.component';
 import { BlogPostComponent } from './components/blog-post/blog-post.component';
 import { TemplateComponent } from './components/template/template.component';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LegalComponent } from './pages/legal/legal.component';
+import { RoadmapComponent } from './pages/roadmap/roadmap.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HeaderComponent } from './components/navigation/header/header.component';
+import { FooterComponent } from './components/navigation/footer/footer.component'; // Import HttpClientModule
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogListComponent,
     BlogPostComponent,
-    TemplateComponent
+    TemplateComponent,
+    HomeComponent,
+    AboutComponent,
+    LegalComponent,
+    RoadmapComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
