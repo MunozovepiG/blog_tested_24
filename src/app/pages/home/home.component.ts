1ea6bf4627acb15964585c2c3c31b4088e69dacc
Muno Gwata <munozovepi@astute.money>
2024-03-06 14:42:20 +0400

    with the full header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+question_one: boolean = true;
+question_two: boolean = false;  
+question_three: boolean = false;
+question_four: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showQuestionOne(){
+    this.question_one = true;
+    this.question_two = false;
+    this.question_three = false;
+    this.question_four = false;
+  }
+
+
+  showQuestionTwo(){
+    this.question_one = false;
+    this.question_two = true;
+    this.question_three = false;
+    this.question_four = false;
+  }
+  showQuestionThree(){
+    this.question_one = false;
+    this.question_two = false;
+    this.question_three = true;
+    this.question_four = false;
+  }
+  showQuestionFour(){
+    this.question_one = false;
+    this.question_two = false;
+    this.question_three = false;
+    this.question_four = true;
+  }
 }
